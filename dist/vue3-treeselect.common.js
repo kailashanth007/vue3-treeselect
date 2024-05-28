12d5f7d4705e5df94df13818ad837ad2f6a41bd2
Karthick.raja <karthick.raja@jazenetworks.com>
2024-05-28 12:40:12 +0530

    Treeselect arrow changed

--- dist/vue3-treeselect.common.js
+++ dist/vue3-treeselect.common.js
@@ -7101,11 +7101,12 @@ function MultiValueItemvue_type_script_lang_js_isSlot(s) {
 
 var Arrowvue_type_template_id_5d5151cb_hoisted_1 = {
   xmlns: "http://www.w3.org/2000/svg",
-  viewBox: "0 0 292.362 292.362"
+  viewBox: "0 0 24 24",
+  width: "24", height: "24"
 };
 
 var Arrowvue_type_template_id_5d5151cb_hoisted_2 = /*#__PURE__*/Object(external_commonjs_vue_commonjs2_vue_root_Vue_["createVNode"])("path", {
-  d: "M286.935 69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952 0-9.233 1.807-12.85 5.424C1.807 72.998 0 77.279 0 82.228c0 4.948 1.807 9.229 5.424 12.847l127.907 127.907c3.621 3.617 7.902 5.428 12.85 5.428s9.233-1.811 12.847-5.428L286.935 95.074c3.613-3.617 5.427-7.898 5.427-12.847 0-4.948-1.814-9.229-5.427-12.85z"
+  d: "M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"
 }, null, -1);
 
 function Arrowvue_type_template_id_5d5151cb_render(_ctx, _cache, $props, $setup, $data, $options) {
